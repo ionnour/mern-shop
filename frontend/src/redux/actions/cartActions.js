de69b90aa737f47a0f23deafd703e3306386fbef
Ion Nour <ionnouralpha@gmai.com>
2021-02-27 16:42:03 +0200

    add actions and reducers for cart

---
+++ frontend/src/redux/actions/cartActions.js
@@ -0,0 +1,29 @@
+import * as actionTypes from "../constants/cartConstants";
+import axios from "axios";
+
+export const addToCart = (id, qty) => async (dispatch, getState) {
+  const {data} = await axios.get(`/api/products/${id}`);
+
+  dispatch({
+    type: actionTypes.ADD_TO_CART,
+    payLoad: {
+      product: data._id,
+      name: data.name,
+      imageURL: data.imageURL,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+
+  localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: actionTypes.REMOVE_FROM_CART,
+    payLoad: id,
+  });
+
+  localStoragesetItem("cart", JSON.stringify(getState().cart.cartItems));
+}
